8b9d6934c0df0b98a4529e65832824ebb1508ab5
emoretti85 <ettoremoretti27@gmail.com>
2015-11-11 14:29:04 +0100

    Create tableCreate.sql

---
+++ install/tableCreate.sql
@@ -0,0 +1,5 @@
+CREATE TABLE IF NOT EXISTS `clickcounter` (
+`ID` int(11) NOT NULL,
+  `Link` varchar(255) NOT NULL,
+  `Count` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
